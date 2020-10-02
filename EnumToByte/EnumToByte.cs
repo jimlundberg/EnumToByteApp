0243cbd31299790f5c6c3af3592f7e609db0e0fa
JamesRobertLundberg <JamesRobertLundberg@hotmail.com>
2020-10-01 17:57:47 -0700

    Add project files.

---
+++ EnumToByte/EnumToByte.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+enum DebugMode : byte
+{
+    NONE = 0,
+    JOB_LIST = 1,
+    CHECK_FILE = 2,
+    CHECK_TCP_IP = 4
+}
+
+class EnumToByte
+{
+    static void Main(string[] args)
+    {
+        DebugMode debugMode = DebugMode.CHECK_TCP_IP;
+        byte debugByte;
+        string debugString;
+        Console.WriteLine("myDirection = {0}", debugMode);
+        debugByte = (byte)debugMode;
+        debugString = Convert.ToString(debugMode);
+        Console.WriteLine("byte equivalent = {0}", debugByte);
+        Console.WriteLine("string equivalent = {0}", debugString);
+    }
+}
